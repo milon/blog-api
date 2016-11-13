8e74ca75692df817e094bd8922427672724ab3c5
Nuruzzaman Milon <contact@milon.im>
2016-11-13 23:26:51 +0600

    test pagination added

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return $postList = Post::all();
+        return $postList = Post::paginate(10);
     }
 
     /**
